a0446b707fd8a9c404fb68ec0f324ec8b13293b8
unknown <asumona884@gmail.com>
2026-05-02 03:13:13 +0600

    New book released section added

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,23 +1,41 @@
 import Image from 'next/image';
 import React from 'react';
 
+
 const HomeCard = () => {
     return (
-        <div>
-            <div className="card card-side bg-base-100 shadow-sm">
-  <figure>
-    <Image
-      src="https://i.ibb.co/XZDrhYYQ/download-1.jpg" width={300} height={300}
-      alt="Movie" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">New movie is released!</h2>
-    <p>Click the button to watch on Jetflix app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Watch</button>
-    </div>
-  </div>
-</div>
+        <div className='lg:grid lg:grid-cols-2 md:grid md:grid-cols-2  gap-9 grid grid-cols-1 p-5 container mx-auto my-11'>
+
+            
+             <div className="card card-side bg-base-100 shadow-lg container mx-auto ">
+                <figure>
+                    <Image
+                        src="https://i.ibb.co/XZDrhYYQ/download-1.jpg" width={300} height={300}
+                        alt="Movie" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">New Book is released!</h2>
+                    <p className='text-gray-400'>Click the button to Read on EverBooks.</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn bg-pink-900 text-white">Read</button>
+                    </div>
+                </div>
+            </div>
+            <div className="card card-side bg-base-100 shadow-lg container mx-auto">
+                <figure>
+                    <Image
+                        src="https://i.ibb.co/mVm4pfTj/shopping-1.webp" width={300} height={300}
+                        alt="Movie" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">New Book is released!</h2>
+                    <p className='text-gray-400'>Click the button to Read on EverBooks.</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn bg-pink-900 text-white">Read</button>
+                    </div>
+                </div>
+            </div>
+          
         </div>
     );
 };
