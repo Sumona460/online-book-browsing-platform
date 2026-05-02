16f8dde5068ad26aca27f84629d08024b811979d
unknown <asumona884@gmail.com>
2026-05-02 17:41:21 +0600

    section rebuild

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-   images: {
-    domains: ["i.ibb.co"],
+ images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+    ],
   },
+  
   reactCompiler: true,
 };
 
