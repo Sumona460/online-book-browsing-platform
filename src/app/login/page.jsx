a64e87225da39430fcdbf967e29c26fae72854ac
unknown <asumona884@gmail.com>
2026-05-04 00:25:54 +0600

    modify form

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,63 +2,135 @@
 
 import { useRouter } from "next/navigation";
 import GoggleLogin from "../components/GoggleLogin";
+import { useState } from "react";
 
 const LoginPage = () => {
   const router = useRouter();
 
+  const [errors, setErrors] = useState({});
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // fake login (later auth add korba)
-    router.push("/");
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    let newErrors = {};
+
+    // Email validation
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      newErrors.email = "Write a valid email";
+    }
+
+    // Password validation
+    if (password.length < 8) {
+      newErrors.password = "Password at least 8 characters";
+    } else if (!/[A-Z]/.test(password)) {
+      newErrors.password = "need 1 uppercase letter";
+    } else if (!/[0-9]/.test(password)) {
+      newErrors.password = "need minimum 1 number";
+    }
+
+    setErrors(newErrors);
+
+    // No error hole all books page a niye jabe
+    if (Object.keys(newErrors).length === 0) {
+      console.log(email, password);
+      router.push("/allBooks");
+    }
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-
-      {/* Center Card */}
-      <div className="bg-white p-8 rounded-xl shadow-xl w-[90%] md:w-[500px] h-screen my-9">
+      
+      <div className="bg-white p-8 rounded-xl shadow-xl w-[90%] md:w-[500px] my-9">
         
         <h2 className="text-2xl font-bold text-center mb-6">
           Login
         </h2>
 
-        <form onSubmit={handleSubmit}>
+        
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
           
-          <input
-            type="email"
-            placeholder="Email"
-            className="input input-bordered w-full mb-3"
-            required
-          />
+          {/* Email */}
+          <div>
+            <label className="block mb-1 font-medium">Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="john@example.com"
+              className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-pink-900"
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm mt-1">{errors.email}</p>
+            )}
+          </div>
 
-          <input
-            type="password"
-            placeholder="Password"
-            className="input input-bordered w-full mb-4"
-            required
-          />
+          {/* Password */}
+          <div>
+            <label className="block mb-1 font-medium">Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-pink-900"
+            />
+            <p className="text-xs text-gray-500 mt-1">
+              Must be 8+ characters, 1 uppercase & 1 number
+            </p>
+            {errors.password && (
+              <p className="text-red-500 text-sm mt-1">{errors.password}</p>
+            )}
+          </div>
 
-          <button className="btn bg-pink-900 text-white w-full">
-            Submit
-          </button>
+          {/* image */}
+          <div>
+            <label className="block mb-1 font-medium">Image URL</label>
+            <input
+              type="text"
+              name="image"
+             placeholder="Your image url"
+              className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-pink-900"
+            />
+            {errors.image && (
+              <p className="text-red-500 text-sm mt-1">{errors.image}</p>
+            )}
+          </div>
+
+
+          {/* Buttons */}
+          <div className="flex gap-2">
+            <button
+              type="submit"
+              className="bg-pink-900 text-white py-2 rounded-lg w-full"
+            >
+              Submit
+            </button>
+
+            <button
+              type="reset"
+              className="border py-2 rounded-lg w-full"
+            >
+              Reset
+            </button>
+          </div>
         </form>
 
-        
+        {/* Register */}
         <div className="text-center mt-4 text-sm">
           <p>
             Don't have an account?{" "}
             <span
-              onClick={() => router.push("/register")}
+              onClick={() => router.push("/login")}
               className="text-pink-900 cursor-pointer font-medium"
             >
               Register
             </span>
           </p>
         </div>
-<GoggleLogin></GoggleLogin>
-      </div>
 
+        <GoggleLogin />
+      </div>
     </div>
   );
 };
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,10 +1,16 @@
 import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-import { client } from "@/db"; // your mongodb client
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db("online-book-browsing-platform");
 
 export const auth = betterAuth({
-    database: mongodbAdapter(client),
-     emailAndPassword: { 
-    enabled: true, 
-  }, 
+  database: mongodbAdapter(db, {
+    // Optional: if you don't provide a client, database transactions won't be enabled.
+    client
+  }),
+   emailAndPassword: { 
+    enabled: true,
+   }
 });
